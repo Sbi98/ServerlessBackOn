39a409883216300fa4540f848f7fc033b9180bf6
Gennaro Zanfardino <stud.gennaro.zanfardino@gmail.com>
2020-03-28 16:16:56 +0100

    porno

--- api/getAllTasks.js
+++ api/getAllTasks.js
@@ -1,15 +1,22 @@
 const mongoInterface = require('../mongoInterface');
 
 module.exports = (request, response) => {
-  mongoInterface.Task.find().then(
-    (tasks) => {
-      response.status(200).json(tasks);
-    }
-  ).catch(
-    (error) => {
-      response.status(400).json({
-        error: error
-      });
-    }
-  );
+  mongoInterface.Task.aggregate([{
+      $lookup: {
+          from: "users", // collection name in db
+          localField: "neederID",
+          foreignField: "_id",
+          as: "user"
+      }
+      }]).then(
+      (tasks) => {
+        res.status(200).json(tasks);
+      }
+    ).catch(
+      (error) => {
+        res.status(400).json({
+          error: error
+        });
+      }
+    );
 };
