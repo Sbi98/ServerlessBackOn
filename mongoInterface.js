8cc37368993ac2a2ada6bd794519f8e7e67557f9
Gennaro Zanfardino <stud.gennaro.zanfardino@gmail.com>
2020-04-09 21:30:35 +0200

    aa

--- mongoInterface.js
+++ mongoInterface.js
@@ -35,7 +35,7 @@ const userSchema = mongoose.Schema({
   name: { type: String, required: true},
   surname: { type: String, required: true},
   email: { type: String, required: true, unique: true },
-  photo: { type: String, required: true}
+  photo: { type: String, required: true},
   devices: {
     type: Map, // `devices` is a key/value store for string keys
     of: Date // Values must be dates
