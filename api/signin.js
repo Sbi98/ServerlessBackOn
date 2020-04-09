f033e2945034960eea0c61f1d310f24228696be2
Gennaro Zanfardino <stud.gennaro.zanfardino@gmail.com>
2020-04-09 22:49:47 +0200

    add
    add

--- api/signin.js
+++ api/signin.js
@@ -1,5 +1,4 @@
 const mongoInterface = require('../mongoInterface');
-const ObjectId = require('mongodb').ObjectId;
 
 module.exports = (request, response) => {
   var user = new mongoInterface.User({
@@ -12,24 +11,17 @@ module.exports = (request, response) => {
   user.devices.set( request.body.deviceToken , Date.now() ); 
   var dev = 'devices.'.concat(request.body.deviceToken);
   var dt = Date.now()
-  if (user != null){
-    mongoInterface.User.findOneAndUpdate({
-      email: ObjectId(request.body.email)
-    }, {$set: { dev : dt}} 
-    , { upsert: true }
-    , function(err, res) {
-      // Deal with the response data/error
-    });
-  }
-    /*mongoInterface.User.findOne({email : user.email})
+
+  if (user != null)
+    mongoInterface.User.findOne({email : user.email})
     .then(
       (existentuser) => {
         if (existentuser != null) {
-          console.log(existentuser+" already exists");
-          var gdevices = existentuser.devices;
-          gdevices.set( request.body.deviceToken , Date.now() );
-          mongoInterface.User.updateOne({_id : ObjectId(existentuser._id)}, {$set: {'devices' : gdevices}});
+          console.log(existentuser+" already exists")
 
+          gdevices.set( request.body.deviceToken , Date.now() )
+          mongoInterface.User.updateOne({_id : ObjectId(existentuser._id)}, {$set: { dev : dt}});
+          
           response.status(200).json({_id: existentuser._id});
           
           user = null;
@@ -61,7 +53,6 @@ module.exports = (request, response) => {
         });
       }
     );
-    */
   else {
     console.error("Error while creating User. Maybe there are missing fields in the request");
     response.status(400).json({
