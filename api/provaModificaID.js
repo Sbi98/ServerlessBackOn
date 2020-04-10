18779fb14cad51be548b61077d2701a94fac080a
SirGian99 <57813140+SirGian99@users.noreply.github.com>
2020-04-10 02:38:11 +0200

    Update provaModificaID.js

--- api/provaModificaID.js
+++ api/provaModificaID.js
@@ -1,8 +1,9 @@
 const mongoInterface = require('../mongoInterface');
+const ObjectId = require('mongodb').ObjectId;
 
 module.exports = (request, response) => {
   var user = new mongoInterface.User({
-    _id: "7662f3ca693228b1a37cea14e7023a9573ecbd4dce48d0d7b6f0ba31d9e20dea",
+    _id: ObjectId.createFromHexString("7662f3ca693228b1a37cea14e7023a9573ecbd4dce48d0d7b6f0ba31d9e20dea"),
     name: "Gian",
     surname: "Sorr",
     email: "mail@mmail.mail",
